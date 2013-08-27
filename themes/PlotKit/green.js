03496be169c2c3e085715099aba89fa487eae721
Christophe Jolif <cjolif@gmail.com>
2013-08-27 11:17:32 +0200

    cleanups

--- themes/PlotKit/green.js
+++ themes/PlotKit/green.js
@@ -1,4 +1,4 @@
-define(["./base", "../utils"], function(pk, utils){
+define(["./base", "../utils"], function(base, utils){
 	var green = base.clone();
 	green.chart.fill = green.plotarea.fill = "#eff5e6";
 	green.colors = utils.defineColors({hue: 82, saturation: 60, low: 40, high: 88});
